eda4939a2abe11bde83c401a0cf246ee2422ffd0
aamuhammad <aamuhammad@uh.edu>
2021-03-03 10:57:57 -0600

    Part C

--- Homework 2/main.py
+++ Homework 2/main.py
@@ -4,8 +4,9 @@ dates = file1.readlines()
 months = ['January','February','March','April','May','June',
           'July','August','September','October','November','December']
 
-
+count = 0
 for stri in dates:
+    count +1
     if stri=='-1':
         break
 
@@ -23,8 +24,16 @@ for stri in dates:
             if stri.find(months[x])>=0:
                 mo = str(x+1)
                 ans = mo + '/' + d + '/' + y
-                print(ans)
+                if count>0:
+                    file2 = open('parsedDates.txt','a')
+                    file2.write("ans \n")
+                    file2.close()
+                else:
+                    file3 = open('parsedDates.txt','w')
+                    file3.write("ans \n")
+                    file3.close()
                 break
+
     except ValueError:
         continue
 
