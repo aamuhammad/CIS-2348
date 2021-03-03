f5564863bc7654c80b23f740899b210616f8c60f
aamuhammad <aamuhammad@uh.edu>
2021-03-03 10:50:20 -0600

    Part B

--- Homework 2/main.py
+++ Homework 2/main.py
@@ -1,18 +1,18 @@
-var = 1
-months = ['January', 'February', 'March', 'April', 'May', 'June',
-          'July', 'August', 'September', 'October', 'November', 'December']
-while var == 1:
-    try:
-        stri = input("Enter the month day, year: ")
-    except SyntaxError:
-        continue
-    if stri == '-1':
+file1 = open('inputDates.txt','r')
+dates = file1.readlines()
+
+months = ['January','February','March','April','May','June',
+          'July','August','September','October','November','December']
+
+
+for stri in dates:
+    if stri=='-1':
         break
 
     try:
         arr = stri.split()
         digit = len(arr)
-        if digit != 3:
+        if digit!=3:
             continue
         m = arr[0]
         day = arr[1]
@@ -20,10 +20,12 @@ while var == 1:
         d, comma = day.split(',')
 
         for x in range(12):
-            if stri.find(months[x]) >= 0:
-                mo = str(x + 1)
+            if stri.find(months[x])>=0:
+                mo = str(x+1)
                 ans = mo + '/' + d + '/' + y
                 print(ans)
                 break
     except ValueError:
         continue
+
+file1.close()
