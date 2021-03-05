5794460222580687daa66fc12c856b466708758c
aamuhammad <aamuhammad@uh.edu>
2021-03-04 19:44:14 -0600

    ZyLab 8.10

--- Homework 2/ZyLabs/ZyLab_8_10.py
+++ Homework 2/ZyLabs/ZyLab_8_10.py
@@ -1,1 +1,12 @@
-
+user_input = input()
+sWithoutSpaces = ""
+for x in user_input:
+    if x != ' ':
+        sWithoutSpaces += x
+sReverse = ""
+for x in sWithoutSpaces:
+    sReverse = x + sReverse
+if sWithoutSpaces == sReverse:
+    print(user_input + " is a palindrome")
+else:
+    print(user_input + " is not a palindrome")
